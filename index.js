18b64fa887992c22364d48bf01475d68a2e56353
ramiro <rhuayller@agetic.gob.bo>
2019-12-26 19:03:48 -0400

    funcional

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended : false}));
-
+app.use(cors({
+    'Access-Control-Allow-Origin': '*',
+    'methods': 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    'preflightContinue': true,
+    'headers': 'Cache-Control, Pragma, Content-Type, Authorization, Content-Length, X-Requested-With',
+    'Access-Control-Allow-Headers': 'Authorization, Content-Type',
+    'X-Frame-Options': 'SAMEORIGIN'
+  }));
+  
 require('./server/routes')(app);
 
 const PORT = 3456;
